6b3a5a3cfb5a8f06847a66d25e57edf76ed06273
Nico Salm <nico@salm.dev>
2023-09-09 13:33:02 -0500

    p1

--- p1/decode.c
+++ p1/decode.c
@@ -1,8 +1,25 @@
-/**
- * Name: Nico Salm
- * CS login: salm
- * Professor: Mark Mansi
- */
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        decode.c
+// This File:        decode.c
+// Other Files:      N/A
+// Semester:         CS 354 Lecture 003 Fall 2023
+// Instructor:       markm (Mark Mansi)
+// 
+// Author:           Nico Salm
+// Email:            nbsalm@wisc.edu
+// CS Login:         salm
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of 
+//                   of any information you find.
+//////////////////////////// 80 columns wide ///////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright 2019 Jim Skrentny
